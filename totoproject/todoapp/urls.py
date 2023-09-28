8dadd15b0e352c33ca5307f2c364f68ffd0e791f
ASHIK KUNJUMON <messenger.to.the.god001@gmail.com>
2023-09-28 21:18:08 +0530

    Initial commit

---
+++ totoproject/todoapp/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path,include
+
+urlpatterns = [
+
+    path('',views.index,name="index"),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update')
+]
